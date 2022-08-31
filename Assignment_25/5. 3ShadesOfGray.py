51229fc2e32158b108380083544afcb81ca0dc46
Amir Shoja <83409092+Amir-Shoja@users.noreply.github.com>
2022-08-31 21:13:43 +0430

    Update 5. 3ShadesOfGray.py

--- Assignment_25/5. 3ShadesOfGray.py
+++ Assignment_25/5. 3ShadesOfGray.py
@@ -12,13 +12,17 @@ while True:
 
     width, height = frame.shape
 
-    detectArea = frame[(width // 5) * 2 : (width // 5) * 3, (height // 5) * 2 : (height // 5) * 3]
+    detectArea = frame[ (width // 5) * 2 : (width // 5) * 3, (height // 5) * 2 : (height // 5) * 3]
     kernel = np.ones((25, 25)) / 625
-    filter_frame = cv2.filter2D(frame, -1, kernel)
-    filter_frame[(width // 5) * 2 : (width // 5) * 3, (height // 5) * 2 : (height // 5) * 3] = detectArea
+    frame = cv2.filter2D(frame, -1, kernel)
 
-    target = np.average(
-        filter_frame[(width // 5) * 2 : (width // 5) * 3, (height // 5) * 2 : (height // 5) * 3])
+    alpha = 3  # Contrast control (1.0-3.0)
+    beta = 0  # Brightness control (0-100)
+    enhanced_target = cv2.convertScaleAbs(detectArea, alpha=alpha, beta=beta)
+
+    frame[(width // 5) * 2 : (width // 5) * 3, (height // 5) * 2 : (height // 5) * 3] = detectArea
+
+    target = np.average(frame[(width // 5) * 2 : (width // 5) * 3, (height // 5) * 2 : (height // 5) * 3])
     if target <= 85:
         color = "Black"
     elif target > 85 and target <= 150:
@@ -26,8 +30,8 @@ while True:
     elif target > 150:
         color = "White"
 
-    cv2.putText(filter_frame, color, (10, 60), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
-    cv2.imshow("output", filter_frame)
+    cv2.putText(frame, color, (10, 60), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
+    cv2.imshow("output", frame)
 
     key = cv2.waitKey(1)
     if key == 27:
